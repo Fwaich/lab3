ed4605bb66a83812741d59a92e6febb888d7fdda
van <>
2024-12-06 20:12:25 +0200

    fix f: add_element; add f: compare_string,conver_bin, print_building(?); complete f: cmp; now const in english

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
-// int rand_int(int bot, int top);
-// float rand_float(float bot, float top);
-// const char* rand_string(const char* arr[], int len);
-
-const char* developers[] = {"СтройГрад", "ДомБуд", "МонолитПлюс"};
-const char* neighborhoods[] = {"Северный район", "Центральный район", "Западный район"};
-const char* types[] = {"панельный", "кирпичный", "монолитный"};
+const char* developers[] = {"Skyline", "Horizon", "Pinnacle", "Nova", "Evergreen"};
+const char* neighborhoods[] = {"Maplewood", "Cedar", "Bluewater", "Golden", "Silverstone"};
+const char* types[] = {"Monolithic", "Brick", "Panel", "Frame", "Stone"};
 
 typedef struct{
 
@@ -16,8 +13,8 @@ typedef struct{
     const char* neighborhood;
     const char* type;
     int year;
-    int has_lift;
-    int has_trash;
+    short has_lift;
+    short has_trash;
     int apartaments_count;
     int floors_count;
     float avg_area;
@@ -39,7 +36,7 @@ void init_vector(vector* v){
 void add_element(vector* v, building el){
     if (v->size == v->capacity) {
         v->capacity *= 2;
-        v->data = (building *)realloc(v, sizeof(building) * v->capacity);
+        v->data = (building *)realloc(v->data, sizeof(building) * v->capacity);
     }
     v->data[v->size++] = el;
 }
@@ -51,8 +48,48 @@ void clear_vector(vector* v) {
     v->capacity = 0;
 }
 
+int compare_string(const char* st1, const char* st2) {
+    if (st1 == st2){
+        return 0;
+    }
+    char sym1;
+    char sym2;
+    char* abt = "abcdefghijklmnopqrstuvwxyz"; 
+    for (int i = 0; st1[i] != '\0'; i++) { 
+        if  (st2[i] == '\0') {
+            return 1;
+        }
+        sym1 = tolower(st1[i]);
+        sym2 = tolower(st2[i]);
+        if (sym1 != sym2) {
+            break;
+        }
+    }
+
+    for (int i = 0; abt[i] != '\0'; i++) {
+        if (sym1 == abt[i]){
+            return 1;
+        }
+        if (sym2 == abt[i]) {
+            return -1;
+        }
+    }
+}
+
 int cmp(vector* v, int ind1, int ind2){
     int n = 0;
+    n = compare_string(v->data[ind1].developer, v->data[ind2].developer);
+    if (n != 0) {
+        return n;
+    }
+    n = compare_string(v->data[ind1].neighborhood, v->data[ind2].neighborhood);
+    if (n != 0) {
+        return n;
+    }
+    n = compare_string(v->data[ind1].type, v->data[ind1].type);
+    if (n != 0) {
+        return n;
+    }
     if (v->data[ind1].year > v->data[ind2].year){
         n = 1;
     } else if (v->data[ind1].year < v->data[ind2].year) {
@@ -87,10 +124,10 @@ void swap_elements(vector* v, int ind1, int ind2) {
     v->data[ind2] = tmp;
 }
 
-void buble_sort(vector* v) {
+void buble_sort(vector* v, int s) {
     for (int i = 0; i < v->size - 1; i++){
         for (int j = 0; j < v->size - i - 1; j++) {       // возможно стоит i - 2 делать
-            if (v->data[j].year > v->data[j + 1].year) {
+            if (cmp(v, j, j + 1) == s) {
                 swap_elements(v, j, j + 1);
             }
         }
@@ -119,28 +156,47 @@ void generate_building(building* b) {
     b->has_trash = rand_int(0, 1);
     b->apartaments_count = rand_int(10, 200);
     b->floors_count = rand_int(1, 15);
-    b->avg_area = rand_float(10, 50);
+    b->avg_area = rand_float(20, 50);
+}
+
+char* convert_bin(short h) {
+    if (h == 1) {
+        return "YES";
+    } else {
+        return "NO";
+    }
+}
+
+void print_building(building b) {
+    printf("%s, %s, %s, %d, %s, %s, %d, %d, %.3f\n", 
+    b.developer,
+    b.neighborhood,
+    b.type,
+    b.year,
+    convert_bin(b.has_lift),
+    convert_bin(b.has_trash),
+    b.apartaments_count,
+    b.floors_count,
+    b.avg_area
+    );
 }
 
 int main(){
     srand(time(NULL));
-    // printf("hi!\n");
-    // for (int i = 0; i < 100; i++) {
-    //     float num = rand_float(10, 50);
-    //     printf("%f\n", num);
-    // }
-    // int len = sizeof(developers) / sizeof(developers[0]);
-    // const char* word = rand_string(developers, len);
     vector v;
     init_vector(&v);
-    for (int i = 0; i < 11; i++){
+    for (int i = 0; i < 10; i++){
         building b;
         generate_building(&b);
-        add_element(&v, b); //проблема в увеличении памяти для вектора
+        add_element(&v, b);
+        print_building(v.data[i]); 
     }
 
-    for (int i =0; i < 11; i++){
-        printf("%d\n", v.data[i].year);
+    buble_sort(&v, -1); //-1 по возрастанию, 1 наоборот
+    printf("\n");
+
+    for (int i =0; i < 10; i++){
+        print_building(v.data[i]);
     }
     return 0;
 }
